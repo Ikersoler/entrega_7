76eea73d719854c5877a9e10233cab7189814515
Ikersoler <ikersonic@gmail.com>
2024-01-28 12:06:16 +0100

    finiquitao

--- Assets/scripts/entrega_7.cs
+++ Assets/scripts/entrega_7.cs
@@ -1,113 +1,150 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class entrega_7 : MonoBehaviour
 {
+    private int cuntaCincos = 0;
+
+    private int totalSuma= 0;
+
+    private string comprobarA = "pepito grillo era un grillo";
+
+
     void Start()
     {
         //ejrcicio 1
-        
-        for (int i = 2; i <= 100; i += 2)
-        {
-            Debug.Log(i);
-        }
 
+        ejercicio1();
 
 
         //ejercicio 2
-        
-        string[] arrayLetras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
 
-       
-        int contadorVocales = ContarVocales(arrayLetras);
-
-        
-        Debug.Log("En total hay  " + contadorVocales + " vocales");
+        ejercicio2();
 
 
 
         //ejercicio 3
 
+        ejercicio3();
+
+
+        //ejercicio 4 
+
+
+
+        ejercicio4();
+
+
+
+        //ejrcicio 5
+
+
+        ejercicio5();
+
+
+    }
+
+
+
+    private void ejercicio1() 
+    {
+        for (int i = 2; i <= 100; i += 2)
+        {
+            Debug.Log(i);
+        }
+
+    }
+
+    private void ejercicio2()
+    {
+        string[] arrayLetras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+
+
+        int contadorVocales = ContarVocales(arrayLetras);
+
+
+        Debug.Log("En total hay  " + contadorVocales + " vocales");
+
+
+
+
+        int ContarVocales(string[] array)
+        {
+            int contar = 0;
+
+            foreach (string letra in array)
+            {
+
+                if ("aeiouAEIOU".Contains(letra))
+                {
+                    contar++;
+                }
+            }
+
+            return contar;
+
+        }
+
+    }
+
+
+    private void ejercicio3() 
+    {
         for (int i = 10; i > 0; i--)
         {
-            Debug.Log(i + " green bottles Hanging on the wall ");
             Debug.Log(i + " green bottles Hanging on the wall");
 
             if (i == 1)
             {
                 Debug.Log("And if one green bottle Should accidentally fall There'll be no green bottles Hanging on the wall");
-               
+
             }
             else
             {
                 Debug.Log("And if one green bottle Should accidentally fall There'll be " + (i - 1) + " green bottles Hanging on the wall");
-                
+
             }
 
-            
+
         }
-
-
-        //ejercicio 4
-
-
-
-        //ejercicio 5
-
-
-
-
-
-
-
-
-
     }
 
-    //ejercicio 2
-    int ContarVocales(string[] array)
+    private void ejercicio4 ()
     {
-        int contar = 0;
-
-        foreach (string letra in array)
+        while (totalSuma <= 100)
         {
-            
-            if ("aeiouAEIOU".Contains(letra))
+            int numerorandom = Random.Range(1, 11);
+            totalSuma = totalSuma + numerorandom;
+
+            if (numerorandom == 5)
             {
-                contar++;
+                cuntaCincos++;
             }
+            Debug.Log(numerorandom);
         }
-
-        return contar;
-
+        Debug.Log($"las suma del numero es " + totalSuma);
+        Debug.Log($"han salido : " + cuntaCincos);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private void ejercicio5()
+    {
+        for (int i = 0; i < comprobarA.Length; i++)
+        {
+            if (comprobarA[i] == 'a')
+            {
+                break;
+            }
+            else
+            {
+                Debug.Log(comprobarA[i]);
+            }
+        }
+    }
 
 
 }
